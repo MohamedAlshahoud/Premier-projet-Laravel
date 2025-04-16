aecc29f2593582be36c643779692cb6c28c396b8
Mohamed Alshahoud <ay1665ay@gmail.com>
2025-04-16 19:12:12 +0200

    Création de la page d'accueil avec accueilController et une vue Blade

---
+++ resources/views/accueil.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <titl>Page d'accueil</titl>
+    </head>
+    <body>
+        <h1>Bienvenue sur la page d'accueil</h1>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\AccueilController;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', [AccueilController::class, 'index']);
